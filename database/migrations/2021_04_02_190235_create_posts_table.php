d8f8dae769377611a3574ed5346cb0052970d968
Deyi Kong <deyikong@yahoo.com>
2021-04-02 14:18:19 -0500

    added migrations

---
+++ database/migrations/2021_04_02_190235_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('body');
+            $table->unsignedBigInteger('owner_id');
+            $table->unsignedBigInteger('main_page_id');
+
+            $table->foreign('owner_id')
+                ->references('id')->on('users');
+
+            $table->foreign('main_page_id')
+                ->references('id')->on('files');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
